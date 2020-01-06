b5c80a72fb6ee8b7e4852f11970cf272ddf75565
Zachary Lewis <zalmellc@gmail.com>
2020-01-06 10:12:20 -0600

    k

---
+++ reactroutesinit/README.md
@@ -0,0 +1,11 @@
+# reactdemo02
+
+<h1>Demo Node, Jade and React App with MySQL and MongoDb</h1>
+
+NOTE: There is a jade dependency that needs to be replaced manually.  
+
+<PRE>
+npm install constantinople@3.1.1 --save
+npm update
+npm audit fix  
+</PRE>
---
+++ reactroutesinit/app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var mongoose = require('mongoose');
+
+var foodb1 = new mongoose.Schema({
+  id: Number,
+  name: String
+});
+var mymodel = mongoose.model('foodb1', foodb1);
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var zachRouter = require('./routes/zach');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use('/', indexRouter);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', usersRouter);
+app.use('/zach', zachRouter);
+app.use(express.static(path.join(__dirname, 'public')));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
