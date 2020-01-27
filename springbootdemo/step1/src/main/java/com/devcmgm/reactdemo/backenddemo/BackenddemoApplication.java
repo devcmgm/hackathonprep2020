cda6cef0b5933424a9135ad44ca55e18b5fb9157
Zachary Lewis <devcmontgomery@gmail.com>
2020-01-26 18:46:38 -0600

    k

---
+++ springbootdemo/step1/src/main/java/com/devcmgm/reactdemo/backenddemo/BackenddemoApplication.java
@@ -0,0 +1,13 @@
+package com.devcmgm.reactdemo.backenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackenddemoApplication.class, args);
+	}
+
+}
